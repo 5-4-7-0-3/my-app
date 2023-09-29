b0fa25558712fc60b24c1f0ee0b5845729081da2
Freak <39245775+KepKepKep@users.noreply.github.com>
2023-09-30 01:29:14 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TradeForm from './form/tradeForm.js';
 
 function App() {
+  const [balance, setBalance] = useState(100);
+  const [pair, setPair] = useState('BTC/USD');
+  const leverage = 50;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Торговля на рынке</h1>
+      <TradeForm balance={balance} setBalance={setBalance} pair={pair} leverage={leverage} />
     </div>
   );
 }
---
+++ src/form/tradeForm.js
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from 'react';
+import '../styles.css';
+import useWebSocket from 'react-use-websocket';
+
+function TradeForm() {
+  const initialBalance = 100;
+  const leverage = 50; // Плечо x50
+  const [balance, setBalance] = useState(initialBalance);
+  const [btcPrice, setBtcPrice] = useState(0);
+  const [inputAmount, setInputAmount] = useState('');
+  const [showBalance, setShowBalance] = useState(true);
+  const [trades, setTrades] = useState([]);
+
+  const socketUrl = 'wss://stream.binance.com:9443/ws/btcusdt@trade';
+  const {
+    lastMessage,
+  } = useWebSocket(socketUrl, {
+    onOpen: () => console.log('WebSocket Connected'),
+    shouldReconnect: (closeEvent) => true,
+  });
+
+  useEffect(() => {
+    if (lastMessage !== null && lastMessage.data) {
+      const messageData = JSON.parse(lastMessage.data);
+      setBtcPrice(parseFloat(messageData.p));
+    }
+  }, [lastMessage]);
+
+  // Добавляем useEffect для обновления прибыли/убытка при изменении цены BTC
+  useEffect(() => {
+  // Функция для обновления прибыли/убытка в каждой сделке
+    const updateProfitForTrades = () => {
+      setTrades((prevTrades) => {
+        return prevTrades.map((trade) => {
+          if (trade.close) {
+            return trade;
+          }
+          const btcValue = btcPrice * trade.btcAmount;
+          if (trade.type === 'LONG') {
+            // Прибыль для длинной позиции
+            trade.profit = (btcValue - trade.amount).toFixed(2);
+          } else {
+            // Прибыль для короткой позиции
+            trade.profit = (trade.amount - btcValue).toFixed(2);
+          }
+          return trade;
+        });
+      });
+    };
+
+    updateProfitForTrades();
+  }, [btcPrice]);
+
+  const handleTrade = (type) => {
+    const amount = parseFloat(inputAmount);
+    if (isNaN(amount) || amount <= 0) {
+      alert('Введите корректную сумму для покупки.');
+      return;
+    }
+
+    const tradeValue = amount * leverage; // Сумма сделки в USDT с учетом плеча x50
+    const btcValue = tradeValue / btcPrice; // Количество BTC, купленных на сумму USDT
+    const newBalance = balance - amount; // Уменьшаем баланс при открытии сделки
+    setBalance(newBalance);
+    const newTrade = {
+      type: type,
+      amount: tradeValue.toFixed(2), // Сумма сделки в USDT
+      btcAmount: btcValue.toFixed(6), // Количество BTC
+      status: 'Открыто',
+      close: false,
+      profit: '0.00', // Начальное значение прибыли
+      leverage: leverage
+    };
+    setTrades([...trades, newTrade]);
+    setInputAmount('');
+  };
+
+  const handleClose = (index) => {
+    const closedTrade = trades[index];
+    const closeBtcValue = btcPrice * closedTrade.btcAmount; // Сумма сделки при закрытии в USDT
+    const tradeValue = closedTrade.type === 'LONG' ? closeBtcValue - closedTrade.amount : closedTrade.amount - closeBtcValue;
+    const newBalance = balance + tradeValue + (closedTrade.amount / closedTrade.leverage); // Увеличиваем баланс при закрытии сделки
+    setBalance(newBalance);
+    const updatedTrades = trades.map((trade, i) => (i === index ? { ...trade, status: 'Закрыто', close: true } : trade));
+    setTrades(updatedTrades);
+  };
+
+  const formatBalance = () => {
+    const btcValue = (balance / btcPrice).toFixed(6);
+    return `(${balance.toFixed(2)}$ ≈ ${btcValue} BTC)`;
+  };
+
+  return (
+    <div className="trade-form">
+      <h2>Фьючерсные контракты на BTC/USDT</h2>
+      <p>
+        Плечо: x{leverage}
+      </p>
+      Цена BTC: {btcPrice.toFixed(2)}$
+      <div>
+        <input
+          className='input-field'
+          type="number"
+          placeholder="Введите сумму в USDT"
+          value={inputAmount}
+          onChange={(e) => setInputAmount(e.target.value)}
+        />
+      </div>
+      <div className="buttons-container">
+        <button className="long-button" onClick={() => handleTrade('LONG')}>
+          LONG
+        </button>
+        <button className="short-button" onClick={() => handleTrade('SHORT')}>
+          SHORT
+        </button>
+      </div>
+      <p>
+        <br />
+        Баланс: {showBalance ? <span className="visible">{formatBalance()}</span> : '***'}
+        <span className="toggle-eye" onClick={() => setShowBalance(!showBalance)}>
+          {showBalance ? '👁️' : '👁️'}
+        </span>
+      </p>
+      <table className="trade-table">
+        <thead>
+          <tr>
+            <th>Тип</th>
+            <th>Сумма (USDT)</th>
+            <th>Количество BTC</th>
+            <th>Статус</th>
+            <th>Прибыль/Убыток (USDT)</th>
+            <th>Закрыть</th>
+          </tr>
+        </thead>
+        <tbody>
+          {trades.map((trade, index) => (
+            <tr key={index}>
+              <td>{trade.type}</td>
+              <td>{trade.amount}</td>
+              <td>{trade.btcAmount}</td>
+              <td>{trade.status}</td>
+              <td>{trade.profit}</td> {/* Отображаем прибыль/убыток из состояния сделки */}
+              <td>{!trade.close && <button onClick={() => handleClose(index)}>Закрыть</button>}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default TradeForm;
